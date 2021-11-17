3cd84104f2987e41c3eeff740fbdd7ff0e3d63da
Joseph Madras <josephkabemba77@gmail.com>
2021-11-17 01:25:09 +0100

    style: handle hamburger menu with javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+
+hamburger.addEventListener("click", () => {
+  // menu.classList.toggle("show");
+  // alert("Joseph");
+  menu.classList.toggle("show");
+});
